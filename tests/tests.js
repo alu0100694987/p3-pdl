907c9ab584b7b7294f3a956a35d78ae24a9a2500
Cristo González Rodríguez <cristogr.93@gmail.com>
2014-02-19 19:37:37 +0000

    Adición de Test vacíos

---
+++ tests/tests.js
@@ -0,0 +1,12 @@
+
+var assert = chai.assert;
+
+suite('Ficheros Init', function() {
+  
+    test('Prueba: Texto', function() {
+        //original.value = "\"Los\", 10, \"elefantes\"\n\"rosa\", \"efervescentes\", \"beben\"\n\"agua\", \"en\", \"el\"\n\"bosque\", \"negro\", \"()>\"";
+        //calculate();
+        //assert.deepEqual(target.innerHTML, "<table class=\"center\" id=\"result\">\n<tbody><tr> <td>Los </td><td> 10 </td><td>elefantes </td> </tr>\n<tr> <td>rosa </td><td>efervescentes </td><td>beben </td> </tr>\n<tr> <td>agua </td><td>en </td><td>el </td> </tr>\n<tr> <td>bosque </td><td>negro </td><td>()&gt; </td> </tr>\n</tbody></table>");
+    });
+
+});
