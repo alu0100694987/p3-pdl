f835d47efc6418ecf13f11866481e7fe1768d87d
Cristo González Rodríguez <cristogr.93@gmail.com>
2014-02-19 15:55:38 +0000

    Commit Inicial - Ficheros base y README

---
+++ README.md
@@ -0,0 +1,10 @@
+Práctica 3: Ficheros INI
+========================
+
+Cristo González Rodríguez - alu0100694987
+
+Sawan J. Kapai Harpalani
+
+Repositorio: https://github.com/alu0100694987/p3-pdl
+
+GitHub Page: http://alu0100694987.github.io/p3-pdl/
---
+++ ini.js
@@ -0,0 +1,80 @@
+"use strict"; // Use ECMAScript 5 strict mode in browsers that support it
+
+$(document).ready(function() {
+   $("#fileinput").change(calculate);
+});
+
+function calculate(evt) {
+  var f = evt.target.files[0]; 
+
+  if (f) {
+    var r = new FileReader();
+    r.onload = function(e) { 
+      var contents = e.target.result;
+      
+      var tokens = lexer(contents);
+      var pretty = tokensToString(tokens);
+      
+      out.className = 'unhidden';
+      initialinput.innerHTML = contents;
+      finaloutput.innerHTML = pretty;
+    }
+    r.readAsText(f);
+  } else { 
+    alert("Failed to load file");
+  }
+}
+
+var temp = '<li> <span class = "<%= token.type %>"> <%= match %> </span>\n';
+
+function tokensToString(tokens) {
+   var r = '';
+   for(var i=0; i < tokens.length; i++) {
+     var t = tokens[i]
+     var s = JSON.stringify(t, undefined, 2);
+     s = _.template(temp, {token: t, match: s});
+     r += s;
+   }
+   return '<ol>\n'+r+'</ol>';
+}
+
+function lexer(input) {
+  var blanks         = /^\s+/;
+  var iniheader      = /^\[([^\]\r\n]+)\]/;
+  var comments       = /^[;#](.*)/;
+  var nameEqualValue = /^([^=;\r\n]+)=([^;\r\n]*)/;
+  var any            = /^(.|\n)+/;
+
+  var out = [];
+  var m = null;
+
+  while (input != '') {
+    if (m = blanks.exec(input)) {
+      input = input.substr(m.index+m[0].length);
+      out.push({ type : 'blanks', match: m });
+    }
+    else if (m = iniheader.exec(input)) {
+      input = input.substr(m.index+m[0].length);
+      out.push({ type: 'header', match: m });
+    }
+    else if (m = comments.exec(input)) {
+      input = input.substr(m.index+m[0].length);
+      out.push({ type: 'comments', match: m });
+    }
+    else if (m = nameEqualValue.exec(input)) {
+      /* while (match casa con /\\$/) concatena la siguiente línea */
+      input = input.substr(m.index+m[0].length);
+      out.push({ type: 'nameEqualValue', match: m });
+    }
+    else if (m = any.exec(input)) {
+      out.push({ type: 'error', match: m });
+      input = '';
+    }
+    else {
+      alert("Fatal Error!"+substr(input,0,20));
+      input = '';
+    }
+  }
+  return out;
+}
+
